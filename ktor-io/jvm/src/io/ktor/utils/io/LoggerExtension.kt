9e672ecb2deee75ad1b2fa7135303afc0bb7b955
sharp <tanapol.sh@gmail.com>
2022-09-07 10:21:31 +0700

    adds log to byte buffer channel for debug

---
+++ ktor-io/jvm/src/io/ktor/utils/io/LoggerExtension.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2014-2022 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package io.ktor.utils.io
+
+import org.slf4j.*
+
+internal fun Logger.traceFn(instance: Any, method: String, msg: String, vararg args: Any) {
+    if (!this.isTraceEnabled) return
+    this.trace("[@" + System.identityHashCode(instance) + "] " + method + "(): " + msg, args)
+}
+
+internal fun Logger.debugFn(instance: Any, method: String, msg: String, vararg args: Any) {
+    if (!this.isDebugEnabled) return
+    this.debug("[@" + System.identityHashCode(instance) + "] " + method + "(): " + msg, args)
+}
