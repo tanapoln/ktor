c928c9389a57f626c0822f73e647ce00c763f4e3
sharp <tanapol.sh@gmail.com>
2022-09-10 11:24:03 +0700

    fix log vararg

--- ktor-io/jvm/src/io/ktor/utils/io/LoggerExtension.kt
+++ ktor-io/jvm/src/io/ktor/utils/io/LoggerExtension.kt
@@ -8,10 +8,10 @@ import org.slf4j.*
 
 internal fun Logger.traceFn(instance: Any, method: String, msg: String, vararg args: Any) {
     if (!this.isTraceEnabled) return
-    this.trace("[@" + System.identityHashCode(instance) + "] " + method + "(): " + msg, args)
+    this.trace("[@" + System.identityHashCode(instance) + "] " + method + "(): " + msg, *args)
 }
 
 internal fun Logger.debugFn(instance: Any, method: String, msg: String, vararg args: Any) {
     if (!this.isDebugEnabled) return
-    this.debug("[@" + System.identityHashCode(instance) + "] " + method + "(): " + msg, args)
+    this.debug("[@" + System.identityHashCode(instance) + "] " + method + "(): " + msg, *args)
 }
